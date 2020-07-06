cd9027a5263c0b8e1720bf76bc4b8afb6cef70aa
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-07-06 16:29:37 -0300

    feat: initial commit 🚀

---
+++ src/server.js
@@ -0,0 +1,9 @@
+import express from "express";
+
+const app = express();
+
+app.get("/", (req, res) => {
+  res.send("Hello");
+});
+
+app.listen(3333, () => console.log("Server running on http://localhost:3333"));
