8a6e17eb423cd147eaa241190be4090916e77d50
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-07-06 20:10:02 -0300

    feat: loops ejs

--- src/server.js
+++ src/server.js
@@ -7,7 +7,12 @@ app.set("view engine", "ejs");
 app.get("/", (req, res) => {
   let name = "Ilgsson";
   let lang = "Javascript";
-  res.render("index", { name, lang });
+  let showMsg = false;
+  const products = [
+    { name: "Xbox One", price: "1599.99" },
+    { name: "PS4", price: "2199.99" },
+  ];
+  res.render("index", { name, lang, msg: showMsg, products });
 });
 
 app.listen(3333, () => console.log("Server running on http://localhost:3333"));
