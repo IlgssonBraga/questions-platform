3b126e2760888571211b374086b5e66d85d793c0
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-07-06 20:16:46 -0300

    feat: static files

--- src/server.js
+++ src/server.js
@@ -1,13 +1,16 @@
 import express from "express";
+import path from "path";
 
 const app = express();
 
 app.set("view engine", "ejs");
 
+app.use(express.static(path.join(__dirname, "..", "public")));
+
 app.get("/", (req, res) => {
   let name = "Ilgsson";
   let lang = "Javascript";
-  let showMsg = false;
+  let showMsg = true;
   const products = [
     { name: "Xbox One", price: "1599.99" },
     { name: "PS4", price: "2199.99" },
