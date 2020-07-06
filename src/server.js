0008478170570c624a979c5097c21988eb485499
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-07-06 19:53:21 -0300

    feat: javascript variables on ejs

--- src/server.js
+++ src/server.js
@@ -5,7 +5,9 @@ const app = express();
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  res.render("index");
+  let name = "Ilgsson";
+  let lang = "Javascript";
+  res.render("index", { name, lang });
 });
 
 app.listen(3333, () => console.log("Server running on http://localhost:3333"));
