d4c68e7a9786785ebffa0e47815185d9108d8032
Ilgsson Braga <ilgssonbraga@gmail.com>
2020-07-06 21:03:40 -0300

    feat: bootstrap

--- src/server.js
+++ src/server.js
@@ -8,14 +8,11 @@ app.set("view engine", "ejs");
 app.use(express.static(path.join(__dirname, "..", "public")));
 
 app.get("/", (req, res) => {
-  let name = "Ilgsson";
-  let lang = "Javascript";
-  let showMsg = true;
-  const products = [
-    { name: "Xbox One", price: "1599.99" },
-    { name: "PS4", price: "2199.99" },
-  ];
-  res.render("index", { name, lang, msg: showMsg, products });
+  res.render("index");
+});
+
+app.get("/questions", (req, res) => {
+  res.render("question");
 });
 
 app.listen(3333, () => console.log("Server running on http://localhost:3333"));
